8519c74f58f36df8e1f6fad188450a78486f5b4e
Joe <129335973+J7779@users.noreply.github.com>
2025-03-15 15:41:35 -0500

    style change

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,6 @@ import Modal from "./modal.mjs";
 
 loadHeaderFooter();
 
-const title = "🎁 Register Now & Win! 🎁";
-const message =
-  "Sign up on your first visit and get a chance to win premium camping gear – tents, sleeping bags, and more! 🏕️";
-const modal = new Modal(title, message, true);
-modal.ShowModal();
-
 
 
 
